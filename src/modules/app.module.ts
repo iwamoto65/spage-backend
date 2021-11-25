3628eb65096e184a24c0fe135fb198d6546a17ce
iwamoto65 <kosuke00940@icloud.com>
2021-11-26 00:27:06 +0900

    moduleにtypeormをインポートした

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path/posix';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: '3306',
+      username: 'root',
+      password: 'password',
+      database: 'spage',
+      entities: [join(__dirname, '**/**.entity{.js,.ts}')],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

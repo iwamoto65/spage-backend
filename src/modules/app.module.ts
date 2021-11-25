dc4bf74270537f0e90af8b7a495b1d4d1c310d5c
iwamoto65 <kosuke00940@icloud.com>
2021-11-26 00:55:35 +0900

    portが文字列だったので数値に修正した

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -9,7 +9,7 @@ import { join } from 'path/posix';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: '3306',
+      port: 3306,
       username: 'root',
       password: 'password',
       database: 'spage',
